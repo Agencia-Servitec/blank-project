b77d10516bfad046d4da2963708e2c133be64c91
noelMoriano <nmoriano26@gmail.com>
2022-11-06 18:02:16 -0500

    update useApiPhotosGet

--- src/api/useApiPhotosGet.js
+++ src/api/useApiPhotosGet.js
@@ -2,7 +2,7 @@ import React, { useCallback } from "react";
 import { useApiJsonPlaceHolder } from "./useApiJsonPlaceHolder";
 
 export const useApiPhotosGet = () => {
-  const { get, loading, error } = useApiJsonPlaceHolder("/photos");
+  const { get, loading, error } = useApiJsonPlaceHolder("/photos?&_limit=10");
 
   const getPhotos = useCallback(async () => {
     return get();
